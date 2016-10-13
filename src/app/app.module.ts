51eba2b563213e8e0b6091a77e1e7c360721d576
fjcorchado <fj.corchado@soditec.es>
2016-10-13 22:02:17 +0200

    Probado angular2 y comentarios del desscriptor de la aplicación (app.module.ts)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,4 +17,15 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+    /**
+     * Toda aplicación angular tiene un descriptor de la aplicación.
+     * Esta clase nunca tendrá lógiaca. No va a tener implementación. Solamente
+     * sirve para porder colocar la etiqueta NgModule. Este descriptor
+     * indica la siguiente información:
+     * declarations: Se indican los componentes declarados.
+     * imports: Se indican los módulos importados.
+     * bootstrap: Se indica el componente principal.
+     * 
+     */
+}
