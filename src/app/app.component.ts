287639ef2ab71d9648f43b2039375cd5a63658ea
fjcorchado <fj.corchado@soditec.es>
2016-10-17 13:45:59 +0200

    Avanzando curso hasta el minuto 2:07:19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,46 @@ export class AppComponent {
   title:string = 'app works!';
   imgUrl:string = '../assets/angular.png';
   name:string = 'anyBody';
-  
+  nombreClase:string = "red";
+//Es para probar ngIf en el html
+  booleano:boolean = true;
+  redActive:boolean = true; 
+  yellowActive:boolean = false;
+//Array
+  paises = [
+            {desc:'elem1', check:true},
+            {desc:'elem2', check:true},
+            {desc:'elem3', check:false}
+  ];
+//Mapa de clases CSS
+  pclases = {
+          "red": false,
+          "yellow": true
+  };
+//valor de width
+  ancho:number = 20;
+
   setName(name:string){
       this.name = name;
+  }
+  
+  cambiarBooleano(){
+      this.booleano = !this.booleano;
+  }
+  
+  cambiarClase(){
+      this.redActive = !this.redActive;
+      this.yellowActive = !this.yellowActive;
+  }
+  
+  cambiarMapaClases(){
+      this.pclases.red = !this.pclases.red;
+      this.pclases.yellow = !this.pclases.yellow;
+  }
+  
+  estilo(){
+      return {
+          'color': this.redActive? 'red' : 'yellow'
+      }
   }
 }
